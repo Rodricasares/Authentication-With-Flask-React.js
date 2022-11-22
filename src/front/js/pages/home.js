19b41608e8f21baf119602077c23bce1761364f7
Rodricasares <rodrigocantudo@gmail.com>
2022-11-22 09:32:25 +0000

    clean Home page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,22 +6,11 @@ import { Navbar } from "../component/navbar";
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
-  useEffect(() => {
-    actions.loadUser();
-  }, []);
-
   return (
     <div className="container">
       <Navbar />
       <h1 className="text-center text-white mt-5">Welcome to 4Geeks</h1>
       <h3 className="text-center text-danger mt-5">Enjoy the experience !!!</h3>
-
-      <div className="text-light text-center">
-        Nombre:
-        {store.users.map((item, index) => (
-          <div key={index}>{item.name}</div>
-        ))}{" "}
-      </div>
     </div>
   );
 };
